ad7fe52e6d8e0cfb1bb75337cda3e3e0bac8c4b7
Lukas Wieser <lukas.luki.wieser@gmail.com>
2023-06-15 00:11:41 +0200

    implement payout of DualAssetDividendToken

--- contracts/DualAssetDividendToken.sol
+++ contracts/DualAssetDividendToken.sol
@@ -2,10 +2,13 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // keeps track
 // a token that yields dividends.
 // there are 2 assets that can be the dividends
+// does not handle receiving of assets
+// simply call distributeDividendsAsset0() and distributeDividendsAsset0() at the location where dividens are received
 contract DualAssetDividendToken is ERC20 {
     struct Account {
         uint dividendsAsset0;
@@ -14,8 +17,10 @@ contract DualAssetDividendToken is ERC20 {
         uint lastDividendsAsset1;
     }
 
+    IERC20 asset0;
+    IERC20 asset1;
     uint totalDividendsAsset0; // as percentages
-    uint totalDividendsAsset1; // as percentages
+    uint totalDividendsAsset1; 
     mapping(address => Account) accounts;
 
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
@@ -54,5 +59,13 @@ contract DualAssetDividendToken is ERC20 {
         }
     }
 
-    // TODO: payout
+    function payoutDividends() external {
+        updateDividends(msg.sender);
+        uint dividendsAsset0 = accounts[msg.sender].dividendsAsset0;
+        uint dividendsAsset1 = accounts[msg.sender].dividendsAsset1;
+        accounts[msg.sender].dividendsAsset0 = 0;
+        accounts[msg.sender].dividendsAsset1 = 0;
+        asset0.transfer(msg.sender, dividendsAsset0);
+        asset1.transfer(msg.sender, dividendsAsset1);
+    }
 }
