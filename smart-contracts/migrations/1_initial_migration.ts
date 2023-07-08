7d589c7f2a0f58d4c25532fba453c283cc25a102
Lukas Wieser <lukas.luki.wieser@gmail.com>
2023-07-08 19:22:11 +0200

    Change truffle migrations to typescript

--- smart-contracts/migrations/1_initial_migration.ts
+++ smart-contracts/migrations/1_initial_migration.ts
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
 
-module.exports = function(deployer) {
+module.exports = function (deployer) {
   deployer.deploy(Migrations);
-};
+} as Truffle.Migration;
+
+export {};
