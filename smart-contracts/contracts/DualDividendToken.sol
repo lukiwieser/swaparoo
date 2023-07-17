a1f350733281408588eba1e4baf4b15564d0abf2
Lukas Wieser <lukas.luki.wieser@gmail.com>
2023-07-17 17:47:39 +0200

    Add natspec comments to DualDividendToken

--- smart-contracts/contracts/DualDividendToken.sol
+++ smart-contracts/contracts/DualDividendToken.sol
@@ -4,11 +4,32 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-
-// A ERC20 token that yields dividends in 2 assets.
-// dividends can be distributed 2 assets (tokenA & tokenB)
-// it does not handle receiving of assets, for this simply call distributeDividendsA() and distributeDividendsB() at the location where dividens are received
-// TODO: account for rounding errors
+/// @title A ERC20 token that yields dividends in 2 assets
+/// @notice Dividends can be distributed 2 assets (tokenA & tokenB) to users, proportional to their balance
+/// @dev This contract does not handle receiving of assets! 
+/// To distribute dividends, call distributeDividendsA() or distributeDividendsB() at the location where dividends are received.
+/// 
+/// HOW DIVIDENDS ARE STORED:
+///
+/// The dividends of a single user can be represented as a running sum:
+/// D_1 * (TS_1 / B_1) + ... + D_n * (TS_n * B_n)
+/// 
+/// - D_n: The nth received dividend.
+/// - B_n: The user's balance of DualDividendToken at the time of receiving dividend n.
+/// - TS_n: The total supply of DualDividendToken at the time of receiving dividend n.
+/// 
+/// If we assume that the balance B_n stays the same during the receiving of tokens, the formula simplifies to:
+/// [(D_1 / TS_1) + ... + (D_n / TS_n)] * B
+/// 
+/// Now we can simply save the dividend percentages (D_1 / TS_1).
+/// 
+/// If we want to calculate a user's dividends, we use the formula: 
+/// (totalDividend - lastDividend) * B
+/// 
+/// - totalDividend: All dividends received so far: (D_1 / TS_1) + ...+ (D_n / TS_n).
+/// - lastDividend: The dividends where the user last paid out.
+/// 
+/// TODO: The contract does not account for lost tokens due to rounding
 contract DualDividendToken is ERC20 {
     struct Account {
         uint dividendsA;
@@ -29,6 +50,7 @@ contract DualDividendToken is ERC20 {
         tokenB = IERC20(_tokenB);
     }
 
+    /// @notice Pays out the dividends to the caller's address
     function payoutDividends() external {
         updateDividends(msg.sender);
         uint dividendsA = accounts[msg.sender].dividendsA;
@@ -39,23 +61,34 @@ contract DualDividendToken is ERC20 {
         tokenB.transfer(msg.sender, dividendsB);
     }
 
+    /// @notice Updates and then returns the dividends in tokenA and tokenB for the caller's address
+    /// Always returns the latest amount of dividends of the caller.
     function getAndUpdateDividends() external returns(uint, uint) {
         updateDividends(msg.sender);
         return (accounts[msg.sender].dividendsA, accounts[msg.sender].dividendsB);
-    }
+    }  
 
+    /// @notice Returns the dividends in tokenA and tokenB for the caller's address. 
+    /// Important: Might return outdated values, since dividends are not updated, when calling this function.
     function getDividends() external view returns(uint, uint) {
         return (accounts[msg.sender].dividendsA, accounts[msg.sender].dividendsB);
     }
 
+    /// @dev Distributes dividends in tokenA based on the specified amount.
+    /// Assumes the caller has already transferred the tokenA to this contract.
     function distributeDividendsA(uint amount) internal {
         totalDividendsA += (amount * MULTIPLIER) / totalSupply();
     }
 
+    /// @dev Distributes dividends in tokenB based on the specified amount.
+    /// Assumes the caller has already transferred the tokenB to this contract.
     function distributeDividendsB(uint amount) internal {
         totalDividendsB += (amount * MULTIPLIER) / totalSupply();
     }
 
+    /// @dev Updates the dividends for the specified account.
+    /// Instead of updating all the users dividends directly when receiving dividends e.g. in distributeDividendsA(), 
+    /// we calculate a user’s dividends in hindsight, thus reducing the amount of work/gas used when distributing dividends.
     function updateDividends(address account) internal {
         uint newDividendsA = totalDividendsA - accounts[account].lastDividendsA;
         uint newDividendsB = totalDividendsB - accounts[account].lastDividendsB;
@@ -70,9 +103,8 @@ contract DualDividendToken is ERC20 {
             accounts[account].lastDividendsB = totalDividendsB;
         }
     }
-	
-    // assmuption: shares of a accounts token stays the same during adding up their dividends
-    // => update dividends if shares of account change
+
+    /// @dev update dividends if shares of account change, due to the assumption that balance of a user stays the same during adding up their dividends
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override {
         super._beforeTokenTransfer(from, to, amount);
         if (from != address(0)) {
